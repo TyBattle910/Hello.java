f721fd89cdfd6dbafedcf968dfac70656b903f22
TyBattle910 <112042994+TyBattle910@users.noreply.github.com>
2022-08-24 21:41:39 -0400

    Add files via upload
    Lab 01 hello java code

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*************************************
+* Notepad++
+**************************************
+* Ty Battle
+* 08/24/2022
+* CMSC 255 and Section 004
+***************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Ty Battle");
+		System.out.println("Notepad++");
+		System.out.println("Lab 01");
+		System.out.println("08/24/2022");
+		System.out.println("CMSC 255 and Section 004ja");
+		System.out.println();
+	}
+}
